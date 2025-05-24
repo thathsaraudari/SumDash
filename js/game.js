6236fe62bf6365944deb27d8d473c7901de49da2
thathsaraudari <105314224+thathsaraudari@users.noreply.github.com>
2025-05-24 14:03:48 +0200

    implemented square left and right

--- js/game.js
+++ js/game.js
@@ -9,7 +9,33 @@
         this.grid.initializeGrid();
         this.generateNewSquare();
         this.startGameLoop();
+        document.addEventListener("keydown", (e) => {
+            
+            const row = this.currentSquare.row;
+            let col = this.currentSquare.col;
+            
+            let nextCol = col;
+
+            if(e.key === "ArrowRight") {
+                console.log("right");
+                nextCol++;
+            } else if( e.key === "ArrowLeft") {
+                console.log("left");
+                nextCol--;
+            }
+
+            
+            if(this.grid.isInsideGrid(row, nextCol) && this.grid.isCellEmpty(row, nextCol)) {
+                this.grid.clearCellValue(row, col);
+                this.currentSquare.col = nextCol;
+                this.grid.setCellValue(row, nextCol, this.currentSquare.value);
+                this.grid.renderGrid();
+            }
+
+
+        });
     }
+    
 
     generateNewSquare() {
         const numberList = [2, 4, 5, 10];
@@ -30,7 +56,9 @@
                 this.grid.clearCellValue(row, col);
                 this.currentSquare.moveDown();
                 this.grid.setCellValue(nextRow, col, value);
-            } 
+            } else {
+                this.generateNewSquare();
+            }
 
             this.grid.renderGrid();
         }, 500)
